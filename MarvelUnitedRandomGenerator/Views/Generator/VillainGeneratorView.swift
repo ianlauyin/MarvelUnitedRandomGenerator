f0c817238d2c0939563c796cb819913a9aab08b3
ianlau <ianlauyin@gmail.com>
2024-07-09 17:12:57 +0800

    Add Villain Generator

---
+++ MarvelUnitedRandomGenerator/Views/Generator/VillainGeneratorView.swift
@@ -0,0 +1,18 @@
+//
+//  VillainGeneratorView.swift
+//  MarvelUnitedRandomGenerator
+//
+//  Created by Ian Lau on 9/7/2024.
+//
+
+import SwiftUI
+
+struct VillainGeneratorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VillainGeneratorView()
+}
