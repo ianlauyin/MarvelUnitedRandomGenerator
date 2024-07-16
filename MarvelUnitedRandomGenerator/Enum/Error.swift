7ea0b03d808a1c5efe82e28df93df022121d95fc
ianlau <ianlauyin@gmail.com>
2024-07-16 10:15:56 +0800

    Refactor ItemView

--- MarvelUnitedRandomGenerator/Enum/Error.swift
+++ MarvelUnitedRandomGenerator/Enum/Error.swift
@@ -2,13 +2,7 @@
 //  Error.swift
 //  MarvelUnitedRandomGenerator
 //
-//  Created by Ian Lau on 5/7/2024.
+//  Created by Ian Lau on 15/7/2024.
 //
 
 import Foundation
-
-enum OperationError : Error{
-    case InsertError
-    case EditError
-    case FetchError
-}
